643f2bc630ef40b532097f60a1e91937c8103f90
natedoggy101 <harrisn8@tcnj.edu>
2016-03-28 18:16:41 -0400

    Update AdvocacyGroup.php

--- app/Model/AdvocacyGroup.php
+++ app/Model/AdvocacyGroup.php
@@ -1,6 +1,8 @@
 <?php
 class Lobbyist extends AppModel {
+    //Name of the advocacy group
     public $name = 'Advocacygroup';
+    //list for the politicians
     public $useTable = 'politicians';
 }
 ?>
