ff3ceca67f33ce64d84f4704d513c4bf3feb8bcf
tesoria1 <tesoria1@tcnj.edu>
2016-04-12 19:26:46 -0400

    Update User.php

--- app/Model/User.php
+++ app/Model/User.php
@@ -23,10 +23,20 @@ class User extends AppModel {
             )
         )
     );
+    /* //commented out to match online version, entered below
     public function beforeSave($options = array()) {            //not in online source
         if (isset($this->data[$this->alias]['password'])) {
             $this->data[$this->alias]['password'] = AuthComponent::password($this->data[$this->alias]['password']); //checks to see if credentials are correct
         }
         return true;
+    }*/
+    public function beforeSave($options = array()) {
+    if (isset($this->data[$this->alias]['password'])) {
+        $passwordHasher = new BlowfishPasswordHasher();
+        $this->data[$this->alias]['password'] = $passwordHasher->hash(
+            $this->data[$this->alias]['password']
+        );
     }
+    return true;
+}
 }
