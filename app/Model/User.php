3bdb1cd53f2db95ed773eea474742d87399e9fd5
tesoria1 <tesoria1@tcnj.edu>
2016-04-12 19:12:39 -0400

    Update User.php

--- app/Model/User.php
+++ app/Model/User.php
@@ -1,7 +1,7 @@
 <?php
 App::uses('AuthComponent', 'Controller/Component');
 class User extends AppModel {
-    public $name = 'User'; //user variable
+    public $name = 'User'; //user variable; not present in online source
     public $validate = array(   //validated through array
         'username' => array(
             'required' => array(
@@ -17,13 +17,13 @@ class User extends AppModel {
         ),
         'role' => array(    
             'valid' => array(   //role is validated through array
-                'rule' => array('inList', array('admin', 'user')),
+                'rule' => array('inList', array('admin', 'user')),      //add journalist
                 'message' => 'Please enter a valid role',
                 'allowEmpty' => false
             )
         )
     );
-    public function beforeSave($options = array()) {
+    public function beforeSave($options = array()) {            //not in online source
         if (isset($this->data[$this->alias]['password'])) {
             $this->data[$this->alias]['password'] = AuthComponent::password($this->data[$this->alias]['password']); //checks to see if credentials are correct
         }
