e3875908f085d8a22dd810479bd7385a00a084c7
gregheitman <heitmag1@tcnj.edu>
2016-04-13 15:04:31 -0400

    Update FlashHelper.php

--- app/View/Helper/FlashHelper.php
+++ app/View/Helper/FlashHelper.php
@@ -6,9 +6,4 @@ class FlashHelper extends AppHelper {
 
 <?php echo $this->Flash->render() ?>
 
-// In your View: Will use great_success.ctp instead of success.ctp
-<?php echo $this->Flash->render('flash', array(
-    'element' => 'great_success'
-)); ?>
-
 }
