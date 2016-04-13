f519daee525f560a0b33b5154bd54f2301de9c97
gregheitman <heitmag1@tcnj.edu>
2016-04-13 14:42:58 -0400

    Update FlashHelper.php

--- app/View/Helper/FlashHelper.php
+++ app/View/Helper/FlashHelper.php
@@ -8,11 +8,6 @@ class FlashHelper extends AppHelper {
 
 <?php echo $this->Flash->render('other') ?>
 
-// In your Controller
-$this->Flash->set('The user has been saved.', array(
-    'element' => 'success'
-));
-
 // In your View: Will use great_success.ctp instead of success.ctp
 <?php echo $this->Flash->render('flash', array(
     'element' => 'great_success'
