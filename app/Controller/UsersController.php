325762e2b101f42d8b907da319830a4149f7241f
kimberly-feldman-42 <kimberly.feldman.42@gmail.com>
2016-04-14 12:06:52 -0400

    Update UsersController.php

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -166,15 +166,15 @@ private function _findOrCreateUser($user_profile = array(), $provider=null) {
     	//$this->Session->setFlash(__('Invalid Username or Password. Please try again.'));	//trying to debug, see what happens here
     	//debug($this->Auth->login()); //still trying to debug
         //if ($this->Auth->login()) {		//this line currently is not functioning; need to satisfy Auth.
-        	if($this->Auth->allow("*")){
+        	$this->Auth->allow("*");
         	$this->Session->setFlash(__('Login Success? '));  ///just trying something
             return $this->redirect($this->Auth->redirectUrl());
-        	}
+        	
         //}
-        else{
+        //else{
         
-       $this->Session->setFlash(__('Invalid Username or Password. Please try again.')); //display this if credentials are incorrect
-        }
+       //$this->Session->setFlash(__('Invalid Username or Password. Please try again.')); //display this if credentials are incorrect
+        //}
        //$this->Flash->error(__('Invalid username or password, try again')); 		//this resulted in error; the error() method was not found
   }
 }
