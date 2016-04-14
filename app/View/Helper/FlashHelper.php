30c2c6a74584540559e454c783ed94d704b366c9
tesoria1 <tesoria1@tcnj.edu>
2016-04-13 22:21:26 -0400

    Update FlashHelper.php

--- app/View/Helper/FlashHelper.php
+++ app/View/Helper/FlashHelper.php
@@ -21,7 +21,7 @@ class FlashHelper extends AppHelper {
 		return $this->render($types);
 	}
 	public function render(array $types = []){
-		$this->Flash->render();
+		return $this->Flash->render();
 	}
 
 
