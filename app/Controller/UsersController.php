37739afa163d882c2f781f87db85d81b648e4673
kimberly-feldman-42 <kimberly.feldman.42@gmail.com>
2016-04-14 12:25:10 -0400

    Update UsersController.php

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -165,8 +165,8 @@ private function _findOrCreateUser($user_profile = array(), $provider=null) {
     if ($this->request->is('post')) {		//not sure what post is; this condition is satisfied though
     	//$this->Session->setFlash(__('Invalid Username or Password. Please try again.'));	//trying to debug, see what happens here
     	//debug($this->Auth->login()); //still trying to debug
-    	$this->Auth->allow("*");
-        if ($this->Auth->login()) {		//this line currently is not functioning; need to satisfy Auth.
+    	//$this->Auth->allow("*");
+        if ($this->Auth->allow("*") {		//this line currently is not functioning; need to satisfy Auth. switching ->login() to ->allow("*")
         	$this->Session->setFlash(__('Login Success! '));  ///just trying something
             //return $this->redirect($this->Auth->redirectUrl());
         	
