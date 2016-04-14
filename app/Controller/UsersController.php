f1de4594bf1330d036edb0daa4bcb43cfa5ea019
kimberly-feldman-42 <kimberly.feldman.42@gmail.com>
2016-04-14 11:15:34 -0400

    Update UsersController.php

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -168,9 +168,13 @@ private function _findOrCreateUser($user_profile = array(), $provider=null) {
         	$this->Session->setFlash(__('Login Success? '));  ///just trying something
             return $this->redirect($this->Auth->redirectUrl());
         }
+        else{
+        	
+        
         $this->Session->setFlash(__('Invalid Username or Password. Try again.'));//display this if credentials are incorrect
         //$this->Flash->error(__('Invalid username or password, try again')); 		//this resulted in error; the error() method was not found
-    }
+  	}
+  }
 }
 
 public function logout() {			//method from online
