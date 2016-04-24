64500511110dd2e08004c2189ee110db607896c1
tesoria1 <tesoria1@tcnj.edu>
2016-04-23 21:40:17 -0400

    Update UsersController.php

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -58,7 +58,7 @@ class UsersController extends AppController {
         	{
         		//this line currently is not functioning; need to satisfy Auth. //removing ->login()
         		$this->Session->setFlash(__('Login Success! '));  ///just trying something
-        		return $this->redirect($this->Auth->redirect());
+        		return $this->redirect($this->Auth->redirect());   //this redirect method currently functions; we tested it by moving it into the else portion, it sent us to the home page and gave us the flash message.
             		//return $this->redirect($this->Auth->redirectUrl());
         	}
         	else
