09f9b2c77dc688fad577209c347d2f7eee4e53d9
tesoria1 <tesoria1@tcnj.edu>
2016-04-23 20:19:11 -0400

    Update AppController.php

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -27,9 +27,9 @@
 //App::uses('Controller', 'Controller');
 
 App::uses('Controller', 'Controller');
-App::uses('FB', 'Facebook.Lib');
-App::uses('FacebookInfo', 'Facebook.Lib');
-App::uses('Facebook', 'Facebook.Lib');
+//App::uses('FB', 'Facebook.Lib');
+//App::uses('FacebookInfo', 'Facebook.Lib');
+//App::uses('Facebook', 'Facebook.Lib');
 
 
 /**
@@ -50,13 +50,13 @@ public $components = array(
     'Session',
     'Auth' => array(
         //loginAction was added to try to start some action. => 'Users', 'users' => 'action', 'authenticate') displays a user table
-        'loginAction' => array('controller' => 'Users', 'users'  => 'action', 'authenticate'), //added this because its going to work
-        //'loginRedirect' => array('controller' => 'pages', 'action' => 'display', 'main'),
-        //'logoutRedirect' => array('controller' => 'pages', 'action' => 'display', 'main'),
+        //'loginAction' => array('controller' => 'Users', 'users'  => 'action', 'authenticate'), //added this because its going to work
+        'loginRedirect' => array('controller' => 'pages', 'action' => 'display', 'main'),     //this was initially not commented.
+        'logoutRedirect' => array('controller' => 'pages', 'action' => 'display', 'main'),    //this was initially not commented.
         //'authorize' => array('Controller'), // Added this line
        // 'authenticate' => array('Form' => array('User' => 'login', 'fields' => array('username' => 'username', 'password' => 'password'))))
-    ),
-    'Facebook.Connect' => array('model' => 'User')
+    )
+    //'Facebook.Connect' => array('model' => 'User')
 );
 
 //Checks to see if user is Admin
