de9084110bbc64b5fcd0ece2dceba8d6d9796f6e
kimberly-feldman-42 <kimberly.feldman.42@gmail.com>
2016-04-25 18:09:59 -0400

    Update UsersController.php

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -49,12 +49,8 @@ class UsersController extends AppController {
     		//more nonsense
     		//$this->User->create();
             	$username = $this->request->data['User']['username'];
-            	$password = $this->request->data['User']['password']; //[User][password];
-            	//$username =
-            	//$password =
-            	//$this->Session->setFlash(__(UserUsername));
-            	//end of more nonsense
-    		
+            	$password = $this->request->data['User']['password']; 
+    		$date = date('m/d/Y h:i:s a', time());
     		
     		
     		
@@ -64,7 +60,7 @@ class UsersController extends AppController {
     			or die('Count not connect: ' . pg_last_error());
     			
     			
-    		$result = pg_query($dbconn, "INSERT INTO newsoap.users(id, username, password) VALUES(5006, '$username', '$password');");
+    		$result = pg_query($dbconn, "INSERT INTO newsoap.users(id, username, password, created, modified) VALUES(5006, '$username', '$password', '$date', '$date');");
     		
     		var_dump($result);
     		
