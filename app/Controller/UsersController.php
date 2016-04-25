8d738fea656b431ccb941f8835ba4b3294d2fd72
kimberly-feldman-42 <kimberly.feldman.42@gmail.com>
2016-04-25 17:28:13 -0400

    Update UsersController.php

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -49,7 +49,7 @@ class UsersController extends AppController {
     		//more nonsense
     		$this->User->create();
             	$post_data = $this->request->data;
-            	$this->Session->setFlash(__(username));
+            	$this->Session->setFlash(__('username'));
             	//end of more nonsense
     		
     		
