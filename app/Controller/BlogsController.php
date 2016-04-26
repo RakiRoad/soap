12dc05785064c9ba590eb0c362e6823634dd58a8
Ryan Haines <hainesr4@csmac13.lions.tcnj.edu>
2016-04-26 00:28:37 -0400

    update Controller

--- app/Controller/BlogsController.php
+++ app/Controller/BlogsController.php
@@ -1,24 +1,49 @@
-<?php 
-
+<?php
 
 class BlogsController extends AppController {
 
 
-	public function index() {
-	
-	}
-	//view function 
-	// Uses an sqll query at least we hope it will grab the feed values and we use in index.ctp of Blogs
-	public function view($blog_id) 
-	{
-		$feed_sql = 'SELECT feeds FROM "newsoap"."feeds"';
-		$feed_info = $this->Feed->query($feed_sql);
-		$this->('feed_info', $feed_info);
-	}
+        public function index() {
+         $feed_sql = 'SELECT ARRAY(SELECT ROW(feeds) FROM "newsoap"."feeds")';
+          $feed_info = $this->Blog->query($feed_sql);
+                  $this->set('feed_info', $feed_info);
 
-   
+
+          }
+
+
+        public function add(){
+
+        }
+
+       public function view(){
+         /*  $feed_sql = 'SELECT feeds FROM "newsoap"."feeds"';
+           $feed_info = $this->Feed->query($feed_sql);
+           $this->set('feed_info', $feed_info);*/
+           }
+        public function blog(){
+           $blog_sql = 'SELECT * FROM "newsoap"."blogs"';
+           $blog_info = $this->Blog->query($blog_sql);
+           $this->set('blog_info', $blog_info);
+}
+
+        public function category(){
+           $category_sql = 'SELECT * FROM "newsoap"."categories"';
+           $category_info = $this->Category->query($category_sql);
+           $this->set('category_info', $category_info);
+}
+
+        public function comment(){
+           $comment_sql = 'SELECT * FROM "newsoap"."comments"';
+           $comment_info = $this->Comment->query($comment_sql);
+           $this->set('comment_info', $comment_info);
+}
 
 }
 
 
+
+
+
 ?>
+
