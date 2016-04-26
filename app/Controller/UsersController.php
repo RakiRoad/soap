0dc26b63e6cfeec088ec12adddaac7bde375ad4a
kimberly-feldman-42 <kimberly.feldman.42@gmail.com>
2016-04-25 20:13:38 -0400

    Update UsersController.php

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -59,7 +59,7 @@ class UsersController extends AppController {
             	$dbconn = pg_connect("host=localhost port= 5432 dbname=soap user=postgres password=cabect")
     			or die('Count not connect: ' . pg_last_error());
             	$result = pg_query($dbconn, "SELECT password FROM newsoap.users WHERE username = '$username'")
-            	or die('Username may already be in user: ' . pg_last_error());
+            	        or die('Username may already be in user: ' . pg_last_error());
             	$this->Session->setFlash($username);
             	
             	var_dump($result);
